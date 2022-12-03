9b233191277d98763e588d30be09a65dc95a9544
yash-1708 <yashbhandare1708@gmail.com>
2022-12-03 13:01:59 -0500

    video subtitle API problems fixed and added to the main backend file

--- whisper-playground-main/backend/app.py
+++ whisper-playground-main/backend/app.py
@@ -1,9 +1,12 @@
 import os
 import tempfile
+import ffmpeg
 import flask
 from flask import request
 from flask_cors import CORS
 import whisper
+import subprocess
+from pydub.utils import mediainfo
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -34,4 +37,50 @@ def transcribe():
         return result['text']
     else:
         return "This endpoint only processes POST wav blob"
+
+@app.route('/videosub', methods=['POST'])
+def subtitle_the_video():
+    print(request.form)
+    print(request.files)
+    print(request.method)
     
+    if request.method == 'POST':
+         language = request.form['language']
+         input_video = request.files['video']
+         model = request.form['model_size']
+         input_video.save(input_video.filename)
+    print(language,input_video,model)
+    
+    if model != 'large' and language == 'english':
+            model = model + '.en'
+    audio_model = whisper.load_model(model)
+    print(audio_model.device)
+    def video2mp3(video_file, output_ext="mp3"):
+        filename, ext = os.path.splitext(video_file)
+        subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"], 
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT)
+        return f"{filename}.{output_ext}"
+    
+    audio_file = video2mp3(input_video.filename)
+    print(audio_file)
+    audio_file_info = mediainfo(audio_file)
+    print(audio_file_info)
+    
+    def translate(audio):
+        options = dict(beam_size=5, best_of=5)
+        translate_options = dict(task="translate", **options)
+        result = audio_model.transcribe(audio_file,**translate_options)
+        return result
+    
+    result = translate(audio_file)
+    print(result["text"])
+    audio_path = audio_file.split(".")[0]
+    with open(os.path.join(audio_path + ".vtt"), "w") as vtt:
+        whisper.utils.write_vtt(result["segments"], file=vtt)
+    subtitle = audio_path + ".vtt"
+    output_video = audio_path + "_subtitled.mp4"
+    video = ffmpeg.input(input_video.filename)
+    audio = video.audio
+    ffmpeg.concat(video.filter("subtitles", subtitle), audio, v=1, a=1).output(output_video).run()
+    return subtitle
