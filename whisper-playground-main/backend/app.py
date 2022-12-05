5e5e6dfef98e36ce528ef0a73dbc727b42f59fe1
Winnie Har <winniehar@gmail.com>
2022-12-06 01:08:19 +0800

    Bug fixes
    Revert to save path
    Added checks to prevent gTTS from crashing

--- whisper-playground-main/backend/app.py
+++ whisper-playground-main/backend/app.py
@@ -44,6 +44,7 @@ def transcribe():
 
 @app.route('/transcribe2', methods=['POST'])
 def transcribe2():
+    # Only works when language selected is "english"
     if request.method == 'POST':
         language = request.form['language']
         model = request.form['model_size']
@@ -53,24 +54,30 @@ def transcribe2():
             model = model + '.en'
         audio_model = whisper.load_model(model)
 
-        print("Say something!")
+        temp_dir = tempfile.mkdtemp()
+        save_path = os.path.join(temp_dir, 'temp.wav')
+
+        wav_file = request.files['audio_data']
+        wav_file.save(save_path)
+
         pygame.init()
         pygame.mixer.init()
         options = dict(beam_size=5, best_of=5)
         translate_options = dict(task="translate", **options)
         mp3_player = BytesIO()
-        # get audio
-        audio = request.files['audio_data']
-        torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
 
-        result = audio_model.transcribe(torch_audio, **translate_options)
+        result = audio_model.transcribe(save_path, **translate_options)
 
         predicted_text = result["text"]
-        tts = gTTS(result["text"], lang='en')
-        tts.write_to_fp(mp3_player)
-        pygame.mixer.music.load(mp3_player, 'mp3')
-        pygame.mixer.music.play()
-        print("You said: " + predicted_text)
+
+        # Only send to gTTS if the string has alphabets, otherwise backend will crash
+        if predicted_text and re.sub(r'[^a-zA-Z]', '', predicted_text):
+            tts = gTTS(result["text"], lang='en')
+            tts.write_to_fp(mp3_player)
+            pygame.mixer.music.load(mp3_player, 'mp3')
+            pygame.mixer.music.play()
+            print("You said: " + predicted_text)
+
         return(predicted_text)
 
 
