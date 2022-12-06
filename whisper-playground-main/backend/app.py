316c2f3cf9637dfb19d2a41f5a757c4daaa635c2
Winnie Har <winniehar@gmail.com>
2022-12-06 14:36:44 +0800

    Removed unused API
    removed transcirbe()

--- whisper-playground-main/backend/app.py
+++ whisper-playground-main/backend/app.py
@@ -17,33 +17,6 @@ from pydub.utils import mediainfo
 app = flask.Flask(__name__)
 CORS(app)
 
-
-@app.route('/transcribe', methods=['POST'])
-def transcribe():
-    if request.method == 'POST':
-        language = request.form['language']
-        model = request.form['model_size']
-
-        # there are no english models for large
-        if model != 'large' and language == 'english':
-            model = model + '.en'
-        audio_model = whisper.load_model(model)
-
-        temp_dir = tempfile.mkdtemp()
-        save_path = os.path.join(temp_dir, 'temp.wav')
-
-        wav_file = request.files['audio_data']
-        wav_file.save(save_path)
-
-        if language == 'english':
-            result = audio_model.transcribe(save_path, language='english')
-        else:
-            result = audio_model.transcribe(save_path)
-
-        return result['text']
-    else:
-        return "This endpoint only processes POST wav blob"
-
 @app.route('/transcribe2', methods=['POST'])
 def transcribe2():
     # Only works when language selected is "english"
